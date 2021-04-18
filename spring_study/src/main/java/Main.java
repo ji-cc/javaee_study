51968a30eea0177438d06a7ee3d222c19cdd4e76
ji-cc <jicc0610@163.com>
2021-04-18 13:37:32 +0800

    提交

--- spring_study/src/main/java/Main.java
+++ spring_study/src/main/java/Main.java
@@ -1,3 +1,8 @@
+
+import duck.Duck;
+import duck.Duck2;
+import duck.Duck3;
+import duck.DuckShop;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,5 +15,21 @@ public class Main {
          */
         ApplicationContext context = new
                 ClassPathXmlApplicationContext("applications.xml");
+
+        Duck d1 = (Duck) context.getBean("d1");
+        System.out.println(d1);   // Duck{name='duck', age=12}
+
+        Duck2 d2 = (Duck2) context.getBean("d2");
+        System.out.println(d2);  // Duck{name='烤鸭', age=2}
+
+        Duck3 d3 = (Duck3) context.getBean("dk1");
+        System.out.println(d3);
+        // Duck3{name='北京烤鸭', age=2, next=Duck3{name='甜皮鸭', age=1, next=Duck3{name='酱鸭', age=3, next=null}}}
+
+        DuckShop duckShop = (DuckShop) context.getBean("shop");
+        System.out.println(duckShop);
+        // DuckShop{ducks=[Duck{name='s1', age=12}, Duck{name='s2', age=12}, Duck{name='s3', age=12}]}
+
+
     }
 }
---
+++ springboot-study/src/main/java/example/springboot_study/SpringbootStudyApplication.java
@@ -0,0 +1,13 @@
+package example.springboot_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStudyApplication.class, args);
+    }
+
+}
