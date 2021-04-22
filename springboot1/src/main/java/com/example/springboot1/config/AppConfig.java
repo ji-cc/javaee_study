853178719243aaea9fe5295e54d33cb6dce353ad
ji-cc <jicc0610@163.com>
2021-04-22 11:11:57 +0800

    提交

--- springboot1/src/main/java/com/example/springboot1/config/AppConfig.java
+++ springboot1/src/main/java/com/example/springboot1/config/AppConfig.java
@@ -1,14 +1,38 @@
 package com.example.springboot1.config;
 
+import com.example.springboot1.config.interceptor.LoginInterceptor;
 import com.example.springboot1.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer { // web框架，执行初始化工作的时候会调用接口方法
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    /*
+    添加web配置：添加拦截器（根据路径进行拦截）
+    注意：/** 表示模糊匹配拦截
+    /*:代表一级路径 ，如/user/*,可以匹配到/user/abc，不能匹配到/user/abc/1
+    /**:代表多级的路径
+    静态资源也会被拦截的
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+         // 实现用户会话管理的功能
+        // 定义的拦截器要属于 HandlerInterceptor 接口
+        registry.addInterceptor(new LoginInterceptor(objectMapper))  // 拦截器  链式方法设计，当前类型的方法，返回值就是this
+                .addPathPatterns("/user/**")   // 添加要拦截的路径
+                .excludePathPatterns("/user/login");  // 排除的路径
+    }
+
     @Bean
     public Map<Integer,Integer> test1() {
         Map<Integer,Integer> map = new HashMap<>();
--- springboot1/src/main/java/com/example/springboot1/controller/UserController.java
+++ springboot1/src/main/java/com/example/springboot1/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 //当前类型注册实例到容器中，并指定为Web请求的处理
@@ -17,14 +18,14 @@ import java.util.Map;
 //@RequestMapping可以定义请求相关的配置：如路径，请求方法等等
 @RequestMapping("/user")
 public class UserController {
-    @Autowired
-    private Map<Integer,Integer> test1;
-
-    @RequestMapping("test8")
-    @ResponseBody
-    public Object test8() {
-        throw new RuntimeException("数据库报错了");
-    }
+//    @Autowired
+//    private Map<Integer,Integer> test1;
+//
+//    @RequestMapping("test8")
+//    @ResponseBody
+//    public Object test8() {
+//        throw new RuntimeException("数据库报错了");
+//    }
 
 
 
@@ -45,29 +46,13 @@ public class UserController {
 
 
 
-    @RequestMapping("test6")
-    @ResponseBody
-    //  http 请求是基于 Servlet 的，Spring 已经生成了 request 和 response对象，可以直接在参数中使用
-    public Object test6(HttpServletRequest req, HttpServletResponse res) {// Spring自动的将对象注入到参数
-        System.out.println(req.getParameter("username"));
-        System.out.println(req.getParameter("password"));
-        return null;
-    }
-
-
-
-
-//    @Autowired
-//    private Map<Integer,Integer> test1;
-//    @RequestMapping("/test4")
+//    @RequestMapping("test6")
 //    @ResponseBody
-//    public Object test4(){
-//        return null;   //响应体为空
-//    }
-//    @RequestMapping("/test5")
-//    @ResponseBody
-//    public Object test5(){
-//        return "ok";  // 返回字符串 ok，响应体不是返回 json格式，而是字符串内容
+//    //  http 请求是基于 Servlet 的，Spring 已经生成了 request 和 response对象，可以直接在参数中使用
+//    public Object test6(HttpServletRequest req, HttpServletResponse res) {// Spring自动的将对象注入到参数
+//        System.out.println(req.getParameter("username"));
+//        System.out.println(req.getParameter("password"));
+//        return null;
 //    }
 
 
@@ -112,10 +97,7 @@ public class UserController {
 //        return test1;
 //    }
 //
-//    @RequestMapping("/m")
-//    public String m() {
-//        return "/main.html";
-//    }
+
 
     // @Autowired 和 @Resource都可以装配 Bean
 //    @Autowired
@@ -126,19 +108,49 @@ public class UserController {
 //    @Qualifier("user1")
 //    private User user;  // // 变量名和 bean 的名称不一致，@Autowired要和 @Qualifier指定的bean名字搭配
 
-//    @Resource(name = "user1")  // 变量名和bean的名称不一致， @Resource要指定name为bean的名称
-//    private User user;
-//    @RequestMapping("/login")
-//    @ResponseBody
-//    public Object login() {
-//        return user;
-//    }
-//    @RequestMapping("/tt1")
-//    public String tt1() {
-//        return "forward:/user/login";   // 路径的转发
-//    }
-//    @RequestMapping("/tt2")
-//    public String tt2() {
-//        return "redirect:/user/login";   // 路径的重定向
-//    }
+    @Resource(name = "user1")  // 变量名和bean的名称不一致， @Resource要指定name为bean的名称
+    private User user;
+    @RequestMapping("/login")
+    @ResponseBody
+    public Object login(User user,HttpServletRequest req) {
+        // 模拟用户登录，在数据库中校验密码
+        if(!"abc".equals(user.getUsername())) {
+            throw new RuntimeException("用户登录失败");
+        }
+        HttpSession session = req.getSession();
+        session.setAttribute("user",user);
+        return user;
+    }
+    @RequestMapping("/m")
+    public String m() {
+        return "/main.html";
+    }
+
+
+    @RequestMapping("/tt1")
+    public String tt1() {
+        return "forward:/user/login";   // 路径的转发
+    }
+    @RequestMapping("/tt2")
+    public String tt2() {
+        return "redirect:/user/login";   // 路径的重定向
+    }
+
+    @Autowired
+    private Map<Integer,Integer> test1;
+    @RequestMapping("/test4")
+    @ResponseBody
+    public Object test4(){
+        // 配置 ControllerAdvice 时，不会进入重写响应体的方法
+        return null;   //响应体为空
+    }
+    @RequestMapping("/test5")
+    @ResponseBody
+    public Object test5(){
+        return "ok";  // 返回字符串 ok，响应体不是返回 json格式，而是字符串内容
+    }
+
+
+
+
 }
